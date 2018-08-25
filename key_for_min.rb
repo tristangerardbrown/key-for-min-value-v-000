671fa1bf3b617133ca5fb387e411a57fd9a1f7ff
Tristan Brown <tgbrown@stanford.edu>
2018-08-25 17:34:13 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+ikea = {:chair => 25, :table => 85, :mattress => 450}
 
 def key_for_min_value(name_hash)
-
+ikea.collect do |item, price|
+  price.sort {|a, b| a <=> b}
 end
+ikea[1]
+end 
