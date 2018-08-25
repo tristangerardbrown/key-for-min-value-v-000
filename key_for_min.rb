87927e55dabfdd366a0f07de0c506f154ed2174c
Tristan Brown <tgbrown@stanford.edu>
2018-08-25 17:38:21 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,10 +1,10 @@
 name_hash = {:chair => 25, :table => 85, :mattress => 450}
 
 def key_for_min_value(name_hash)
-ikea.collect do |item, price|
+name_hash.collect do |item, price|
   price.sort {|a, b| a <=> b}
 end
-ikea[0]
+name_hash[0]
 end
 
 key_for_min_value(name_hash)
